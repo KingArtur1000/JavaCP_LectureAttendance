230f8b2fb4b9b03adcb7b45d4319ff4603a384d3
Артур Александрович <artur.dm.2005@yandex.ru>
2025-11-18 19:18:12 +0300

    Added Red Color

--- src/main/java/com/kingartur1000/UI/AttendanceMarkRenderer.java
+++ src/main/java/com/kingartur1000/UI/AttendanceMarkRenderer.java
@@ -8,34 +8,72 @@ import java.awt.*;
 
 public class AttendanceMarkRenderer extends JCheckBox implements TableCellRenderer {
 
+    private static final Icon BLACK_CHECK = new ColoredCheckIcon(Color.BLACK);
+    private static final Icon RED_CHECK   = new ColoredCheckIcon(Color.RED);
+
     public AttendanceMarkRenderer() {
         setHorizontalAlignment(CENTER);
+        setOpaque(true);
     }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value,
                                                    boolean isSelected, boolean hasFocus,
                                                    int row, int column) {
-        setOpaque(true);
         setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
 
         if (value instanceof AttendanceMark mark) {
             switch (mark) {
                 case PRESENT -> {
                     setSelected(true);
-                    setForeground(Color.BLACK);
+                    setIcon(BLACK_CHECK);
                 }
                 case LATE -> {
                     setSelected(true);
-                    setForeground(Color.RED);
+                    setIcon(RED_CHECK);
                 }
                 case ABSENT -> {
                     setSelected(false);
-                    setForeground(Color.BLACK);
+                    setIcon(null);
                 }
             }
         }
-
         return this;
+    }
+
+    /**
+     * Внутренний класс для рисования цветной галочки.
+     */
+    static class ColoredCheckIcon implements Icon {
+        private final Color color;
+
+        public ColoredCheckIcon(Color color) {
+            this.color = color;
+        }
+
+        @Override
+        public void paintIcon(Component c, Graphics g, int x, int y) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setColor(color);
+            g2.setStroke(new BasicStroke(2));
+
+            // простая галочка
+            int w = getIconWidth();
+            int h = getIconHeight();
+            g2.drawLine(x + 2, y + h / 2, x + w / 3, y + h - 2);
+            g2.drawLine(x + w / 3, y + h - 2, x + w - 2, y + 2);
+
+            g2.dispose();
+        }
+
+        @Override
+        public int getIconWidth() {
+            return 16;
+        }
+
+        @Override
+        public int getIconHeight() {
+            return 16;
+        }
     }
 }
